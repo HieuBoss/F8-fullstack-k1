1ec1b581ba1d9aa6dc9e2e2f2c03b94e777d2378
HieuBoss <duyhieu06112003@gmail.com>
2023-08-01 14:07:48 +0700

    sua

---
+++ javascript/lesson1/ex1.js
@@ -0,0 +1,20 @@
+/* Bien trong javascript
+
+Nên đặt tiên biến theo quy tắc camelCase
+
+
+
+*/
+// var customer;
+
+// var customerEmail = "Hieu ";
+
+// console.log(customerEmail);
+
+// welcome="<h1>javascript</h1>"
+
+// document.write(welcome);
+// var courseName ="Fullstack";
+// // var Welcome = "Chao mung ban den voi khoa "+ courseName+" f8";
+// var Welcome = `Chao mung ban den voi khoa ${courseName} \`f8\``;
+// console.log(Welcome);
