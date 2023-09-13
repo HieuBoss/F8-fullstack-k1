9ad4e912f0244c63c7af0eb828447fede687fd6a
HieuBoss <duyhieu06112003@gmail.com>
2023-09-13 17:17:49 +0700

    buoi 28

---
+++ javascript_bt/ontap4/ex1/ex1.js
@@ -0,0 +1,11 @@
+/*
+Tạo một hàm tạo có tên Order() đối tượng có tên là order với các thuộc tính sau:
+
+id: id DUY NHẤT đơn hàng, được tạo tự động trong hàm tạo Order().
+
+status: Trạng thái đơn hàng, được tạo tự động trong hàm tạo Order(), giá trị mặc định là "chưa hoàn thành".
+
+product_id: Chứa ID của product
+
+product_quantity: Chứa số lượng của product trong order
+*/
