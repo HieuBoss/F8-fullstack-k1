3f145d17cb61e3b7fec39fc5def4e07435dbbf3b
HieuBoss <duyhieu06112003@gmail.com>
2023-10-31 20:08:11 +0700

    push

--- react_bt/lesson3/src/Service/Config.js
+++
@@ -1,3 +0,0 @@
-export const config = {
-  SERVICE_API: "https://api-todo-ebon.vercel.app/api/v1",
-};
--- react_bt/lesson3/src/pages/Login/login.js
+++
@@ -1,7 +0,0 @@
-import React, { Component } from "react";
-import Form from "./Form";
-export default class login extends Component {
-  render() {
-    return Form();
-  }
-}
